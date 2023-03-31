46c5a76553f1525e987a73b69235430c29b56a78
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2023-03-31 14:14:35 +0800

    updated

--- test3/testre2.py
+++ test3/testre2.py
@@ -1,18 +1,30 @@
+import datetime
 import re2 as re
 import sys
 
 
 
-def main(size,fname):
+def main(size, fname):
+    #  pat = re.compile(r"^(((a)?){1000,1000})((a){1000,1000})$")
+
+    pat = mkpat(size)
+    t1 = time_now = datetime.datetime.now()
     with open(fname, 'r') as f:
         content = f.read()
-        print(re.search(r"^(a?){1000,1000}(a){1000,1000}$", content.strip('\r\n')))
+        m = pat.match(content.strip('\r\n'))
+        print(m.groups())
+    t2 = time_now = datetime.datetime.now()
+    print(t2-t1)
+
+def mkpat(size):
+    size = int(size)
+    pat_str = "^" + ("((a)?)" * size) + ("(a)" * size) + "$"
+    pat = re.compile(pat_str)
+    return pat
 
 
 
-
-
-if len(sys.argv) < 2:
-    print("usage: re2.py filename")
+if len(sys.argv) < 3:
+    print("usage: re2.py num filename")
 else:
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
